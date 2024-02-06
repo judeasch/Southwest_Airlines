7f70a2b08bf61462eb820ea2e85c96d48b1fa5ca
judeasch <judeasch@gmail.com>
2024-02-06 09:34:35 -0600

    Add project files.

---
+++ Pages/Seats/SeatModal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Southwest_Airlines.Pages.Seats
+{
+    public class SeatModalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
