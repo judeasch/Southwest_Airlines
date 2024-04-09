23a06d637631165fb9f0f466248a3b45d70d7c84
Saraswoti Thapa <sthapa4@my.stlcc.edu>
2024-04-09 09:50:08 -0500

    Models change

---
+++ Southwest_Airlines.Data/Models/PaymentInfo.cs
@@ -0,0 +1,36 @@
+﻿using Southwest_Airlines.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Southwest_Airlines.Data.Models
+{
+    internal class PaymentInfo
+    {
+    }
+}
+public class PaymentInfo
+{
+    public int PaymentInfoId { get; set; }
+    public int CustomerId { get; set; }
+    public string CardholderName { get; set; }
+    public string? PaymentMethod { get; set; } 
+    public string? CardNumber { get; set; } 
+    public string? ExpiryDate { get; set; } 
+
+    public virtual Customer Customer { get; set; }
+
+    // Constructor
+    public PaymentInfo(int customerId, string paymentMethod, string cardholderName, string cardNumber, string expiryDate)
+    {
+        CustomerId = customerId;
+        PaymentMethod = paymentMethod;
+        CardholderName = cardholderName;
+        CardNumber = cardNumber;
+        ExpiryDate = expiryDate;
+       
+       
+    }
+}
